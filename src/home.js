105af3435007bb774abc1db6a58cac4fcc6de842
Justin Hairston <jhairst2@vols.utk.edu>
2022-10-07 20:06:29 -0400

    Create header in home.js

--- src/home.js
+++ src/home.js
@@ -1,5 +1,34 @@
 
 export const homePageMod = (() => {
-    
+    const content = document.querySelector("#content");
 
+    function makeHeader() {
+        const header = document.createElement("header");
+        header.classList.add("header-container");
+        return header
+    }
+
+    function addHeaderContent() {
+
+        const headerTitle = document.createElement("span");
+        headerTitle.textContent = "Waffle World";
+
+        const tabList = document.createElement("ul");
+        tabList.classList.add("tab-container");
+
+        const tab1 = document.createElement('li');
+        const tab2 = document.createElement('li');
+        const tab3 = document.createElement('li');
+
+        tab1.textContent = "Home";
+        tab2.textContent = "Menu";
+        tab3.textContent = "About";
+        tabList.append(tab1, tab2, tab3);
+
+        return makeHeader().append(headerTitle, tabList);
+    }
+
+    function createHomePage() {
+        return content.append(addHeaderContent);
+    }
 })
